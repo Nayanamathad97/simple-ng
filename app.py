4c7f7f9bde57c4d0ca90d54b26d1cc597dd10752
Nayanamathad97 <nayanamathad97@gmail.com>
2026-04-25 15:03:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def get_ip():
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
-    return f"The IP address is: {ip}"
+    return f"The IP address is: {ip} and deployed from jenkins Pipeline"
 
-@app.route("/greet")
+@app.route("/nayana")
 def say_hello():
-    return "Hi how are you"
+    return "Hello Ankuj Sir, I have deployed from Jenkins Pipeline and pushed the image to my Docker Hub as well"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
